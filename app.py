ea8452bfb1637ae8042d06f3f9d5d1185f180acf
edijemeni <e.dijemeni@gmail.com>
2015-12-19 22:39:42 +0000

    Data model defined

---
+++ app.py
@@ -0,0 +1,12 @@
+
+
+from models.post import Post
+
+__author__ = "edijemeni"
+
+post = Post("Post1 title","Post1 content", "Post1 author" )
+post2 = Post("Post2 title","Post2 content", "Post2 author" )
+post2.content = "Some different content"
+
+print (post.content)
+print (post2.content)
