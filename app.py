dcfcb0aa2085e6bec30464c9e8b5a639589608f9
edijemeni <e.dijemeni@gmail.com>
2015-12-22 00:24:15 +0000

    Finalising the menu class

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-
-
-from models.post import Post
+from database import  Database
+from models.blog import Blog
 
 __author__ = "edijemeni"
 
-post = Post("Post1 title","Post1 content", "Post1 author" )
-post2 = Post("Post2 title","Post2 content", "Post2 author" )
-post2.content = "Some different content"
 
-print (post.content)
-print (post2.content)
+Database.initialize()
+
+
+blog = Blog(author="Esuabom",
+            title="Sample title",
+            description = "Sample description")
+
+blog.new_post()
+
+blog.save_to_mongo()
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
+
+
--- models/post.py
+++ models/post.py
@@ -1,17 +1,21 @@
+import uuid
+from database import Database
+import datetime
+
 __author__ = 'edijemeni'
 
 
 class Post(object):
-    def __init__(self, blog_id, title, content, author, date, id):
+    def __init__(self, blog_id, title, content, author, date = datetime.datetime.utcnow(), id = None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author =  author
         self.created_date = date
-        self.id = id
+        self.id = uuid.uuid4().hex if id is None else id
 
     def save_to_mongo(self):
-        Database.insert(collection='post',data = self.json())
+        Database.insert(collection='posts',data = self.json())
 
     def json(self):
         return {
@@ -19,6 +23,21 @@ class Post(object):
             'blog_id' : self.blog_id,
             'author' : self.author,
             'content' : self.content,
-            'title' : self.title
+            'title' : self.title,
             'created_date' : self.created_date
         }
+
+    @classmethod
+    def from_mongo(cls, id):
+        post_data = Database.find_one(collection='posts',query={'id':id})
+        return cls(blog_id=post_data['blog_id'],
+                   title=post_data['title'],
+                   content=post_data['content'],
+                   author=post_data['author'],
+                   date=post_data['created_date'],
+                   id= post_data['id'])
+
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={'blog_id': id})]
